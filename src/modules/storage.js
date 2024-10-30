21b0d21a9206844cc73aaac0f25122b63fa92bb7
Vlad_Os <oshovskiyvlad28@gmail.com>
2024-10-30 23:59:16 +0200

    feat: add list and task management methods in storage

--- src/modules/storage.js
+++ src/modules/storage.js
@@ -1,6 +1,7 @@
 import Todo from './todo.js'
 import List from './list.js'
 import Task from './task.js'
+import { lightFormatters } from 'date-fns';
 
 export default class Storage {
   static saveTodoList(data) {
@@ -20,4 +21,47 @@ export default class Storage {
 
     return todoList
   }
+
+  static addList(newList){
+    const todoList = Storage.getTodoList();
+    todoList.addList(newList);
+    Storage.saveTodoList(todoList);
+  }
+
+  static deleteList(listName){
+    const todoList = Storage.getTodoList();
+    todoList.deleteList(listName);
+    Storage.saveTodoList(todoList);
+  }
+
+  static addTask(listName, task){
+    const todoList = Storage.getTodoList();
+    todoList.getList(listName).addTask(task);
+    Storage.saveTodoList(todoList);
+  }
+
+  static deleteTask(listName, taskName){
+    const todoList = Storage.getTodoList();
+    todoList.getList(listName).deleteTask(taskName);
+    Storage.saveTodoList(todoList);
+  }
+
+  static updateTodayList() {
+    const todoList = Storage.getTodoList();
+    todoList.updateTodayList();
+    Storage.saveTodoList(todoList);
+  }
+
+  static updateWeekList() {
+    const todoList = Storage.getTodoList();
+    todoList.updateWeekList();
+    Storage.saveTodoList(todoList);
+  }
+
+  static updateAllTasksList() {
+    const todoList = Storage.getTodoList();
+    todoList.updateAllTasksList();
+    Storage.saveTodoList(todoList);
+  }
+
 }
